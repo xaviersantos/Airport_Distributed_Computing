9c418cbf911893a3ea45cce70431161d64d882d2
Xavier Santos <xavier@ua.pt>
2021-05-20 22:20:18 +0100

    CLEANUP

--- src/airport/InterveningEntities/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Group of the intervening entities in the problem: Passenger, Porter,
- * BusDriver and his states.
- * Each one of them is a separate class. Each entity has a life-cycle, and an
- * internal state that will update itself over time.
- * The states are also separated classes.
- * 
- */
-package airport.InterveningEntities;
--- src/airport/MainProgram/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Main package to execute the program.
- * Contains the main function, as well as a logger for all used entities and
- * a class with all the runtime relevant parameters.
- */
-package airport.MainProgram;
